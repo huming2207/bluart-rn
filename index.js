945e819353467060a338ec78498ef2f94c711632
Jackson Ming Hu <huming2207@gmail.com>
2021-01-17 10:48:18 +1100

    Take out main from index.js

--- index.js
+++ index.js
@@ -1,20 +1,6 @@
-/**
- * @format
- */
-
 import 'react-native-gesture-handler';
-import * as React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-import {Provider as PaperProvider} from 'react-native-paper';
 import {name as appName} from './app.json';
 
-export default function Main() {
-  return (
-    <PaperProvider>
-      <App />
-    </PaperProvider>
-  );
-}
-
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
