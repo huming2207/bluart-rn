549bc8f56a4e71d8314601305860738e6fb0ff0d
Jackson Ming Hu <huming2207@gmail.com>
2021-02-01 22:50:47 +1100

    BluartNavBar: add connection state icon

--- src/components/BluartNavBar/index.tsx
+++ src/components/BluartNavBar/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Appbar, Menu, useTheme } from 'react-native-paper';
+import { Appbar, IconButton, Menu, useTheme } from 'react-native-paper';
 import { StackHeaderProps } from '@react-navigation/stack';
 import { ScreenNames } from '../../screen/common/ScreenNames';
 import { StatusBar } from 'react-native';
+import { Observer } from 'mobx-react-lite';
+import { BleStateInstance } from '../../helper/states/BleState';
 
 export default function BluartNavBar(props: StackHeaderProps): JSX.Element {
   const { previous, navigation } = props;
@@ -10,40 +12,49 @@ export default function BluartNavBar(props: StackHeaderProps): JSX.Element {
   const openMenu = () => setVisible(true);
   const closeMenu = () => setVisible(false);
   const { colors } = useTheme();
+  const bleState = BleStateInstance;
 
   return (
     <>
       <StatusBar backgroundColor={colors.primary} />
-      <Appbar.Header>
-        {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
-        <Appbar.Content title="Bluart" />
-        <Menu
-          visible={visible}
-          onDismiss={closeMenu}
-          anchor={<Appbar.Action icon="menu" color="white" onPress={openMenu} />}>
-          <Menu.Item
-            onPress={() => {
-              navigation.push(ScreenNames.LedControlScreen);
-            }}
-            icon={'led-on'}
-            title="LED Control"
-          />
-          <Menu.Item
-            onPress={() => {
-              console.log('Option 2 was pressed');
-            }}
-            icon={'serial-port'}
-            title="UART Console"
-          />
-          <Menu.Item
-            onPress={() => {
-              navigation.push(ScreenNames.BleConfigScreen);
-            }}
-            icon={'bluetooth'}
-            title="BLE Pairing"
-          />
-        </Menu>
-      </Appbar.Header>
+      <Observer>
+        {() => (
+          <Appbar.Header>
+            {previous ? <Appbar.BackAction onPress={navigation.goBack} /> : null}
+            <Appbar.Content title="Bluart" />
+            <IconButton
+              icon={bleState.selectedDevice ? 'bluetooth-audio' : 'bluetooth-off'}
+              color="white"
+            />
+            <Menu
+              visible={visible}
+              onDismiss={closeMenu}
+              anchor={<Appbar.Action icon="menu" color="white" onPress={openMenu} />}>
+              <Menu.Item
+                onPress={() => {
+                  navigation.push(ScreenNames.LedControlScreen);
+                }}
+                icon={'led-on'}
+                title="LED Control"
+              />
+              <Menu.Item
+                onPress={() => {
+                  console.log('Option 2 was pressed');
+                }}
+                icon={'serial-port'}
+                title="UART Console"
+              />
+              <Menu.Item
+                onPress={() => {
+                  navigation.push(ScreenNames.BleConfigScreen);
+                }}
+                icon={'bluetooth'}
+                title="BLE Pairing"
+              />
+            </Menu>
+          </Appbar.Header>
+        )}
+      </Observer>
     </>
   );
 }
